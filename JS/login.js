3531aba4cc56784adb5900274ca0ca35bcb9d166
AlaaSlalaheldin <alaalolo2266@gmail.com>
2026-05-06 04:49:33 +0300

    login-page1

---
+++ JS/login.js
@@ -0,0 +1,66 @@
+var nameError = document.getElementById("name-error");
+var phoneError = document.getElementById("phone-error");
+var emailError = document.getElementById("email-error");
+var SubmitError = document.getElementById("submit-error");
+
+
+function validateName() {
+    var name = document.getElementById("contact-name").value;
+
+    if(name.length == 0){
+        nameError.innerHTML = 'Name is Required';
+        return false;
+    }
+    if(!name.match(/^[A-Za-z]*\s{1}[A-Za-z]*$/)){
+        nameError.innerHTML = 'write full name';
+        return false;
+    }
+    nameError.innerHTML = '<i class="fa-solid fa-circle-check"></i>';
+    return true;
+}
+
+
+function validatePhone(){
+    var phone = document.getElementById("contact-phone").value;
+
+    if(phone.length == 0){
+        phoneError.innerHTML = 'Phone No. is Required';
+        return false;
+    }
+    if(phone.length !== 10){
+        phoneError.innerHTML = 'phone number should be 11 digits' ;
+        return false;
+    }
+    if(!phone.match(/^[0-9]{10}$/)){
+        phoneError.innerHTML = 'Only digits please';
+        return false;
+    }
+    phoneError.innerHTML = '<i class="fa-solid fa-circle-check"></i>';
+    return true;
+
+}
+
+function validateEmail(){
+    var email = document.getElementById("contact-email").value;
+
+    if(email.length == 0){
+        emailError.innerHTML = 'Email is Required';
+        return false;
+    }
+    if(!email.match (/^[A-Za-z0-9._-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/)){
+        emailError.innerHTML = 'Email Invalid';
+        return false;
+    }
+    emailError.innerHTML = '<i class="fa-solid fa-circle-check"></i>';
+    return true;
+}
+
+function validateForm(){
+    if(!validateName() || !validatePhone() || !validateEmail() || !validateMessage()){
+        SubmitError.style.display = 'block';
+        SubmitError.innerHTML = 'please fix error to submit';
+        setTimeout(function(){SubmitError.style.display = 'none';}, 3000);
+        return false;
+    }
+}
+document.querySelector('.bottom-header').getBoundingClientRect().bottom;
